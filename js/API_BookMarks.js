38a5c56ef5daa53bbd7f8a7df3a2571f857e3a27
M0th1euR0y <116576103+M0th1euR0y@users.noreply.github.com>
2024-09-19 11:45:26 -0400

    Adresse

--- js/API_BookMarks.js
+++ js/API_BookMarks.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/favoris";
+const API_URL = "https://magnetic-ruddy-jelly.glitch.me/api/favoris";
 
 function API_GetFavoris() {
     return new Promise(resolve => {
